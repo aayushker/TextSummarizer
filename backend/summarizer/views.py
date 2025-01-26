be58a234718614752b22b4bc015d6c7ba82fe9eb
aayushker <singhaayushker@gmail.com>
2025-01-27 01:35:02 +0530

    feat(summarizer): Add API view for text summarization with preprocessing and scoring

--- backend/summarizer/views.py
+++ backend/summarizer/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response
+from .process import preProcessing, sentenceScoring, extractTopSentences
 
 # Create your views here.
 
+class SummarizerView(APIView):
+    def post(self, request):
+        data = request.data
+        text = data['text']
+        sentences, words = preProcessing.preProcess_text(text)
+        sentenceScores = sentenceScoring.score_sentences(sentences, words)
+        topSentences = extractTopSentences.summarize_text(sentenceScores)
+        return Response(topSentences)
